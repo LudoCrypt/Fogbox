3eb61bfaa6fc13edb65976c1281725623cb4ca42
LudoCrypt <60561627+LudoCrypt@users.noreply.github.com>
2022-01-07 21:27:04 -0600

    Configg 2

--- src/main/java/net/ludocrypt/fogbox/config/FogboxConfig.java
+++ src/main/java/net/ludocrypt/fogbox/config/FogboxConfig.java
@@ -3,11 +3,13 @@ package net.ludocrypt.fogbox.config;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
+import me.shedaniel.autoconfig.annotation.ConfigEntry;
 import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 
 @Config(name = "fogbox")
 public class FogboxConfig implements ConfigData {
 
+	@ConfigEntry.Gui.RequiresRestart
 	public boolean enableClouds = false;
 
 	public static void init() {
