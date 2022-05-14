446b578be0b78cc7889dd5728cb341fe53fcc7e1
LudoCrypt <60561627+LudoCrypt@users.noreply.github.com>
2022-05-14 15:29:38 -0500

    Fixed sky issue and updated

--- src/main/java/net/ludocrypt/fogbox/Fogbox.java
+++ src/main/java/net/ludocrypt/fogbox/Fogbox.java
@@ -5,7 +5,6 @@ import org.apache.logging.log4j.Logger;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
-import net.ludocrypt.fogbox.config.FogboxConfig;
 import net.ludocrypt.fogbox.shader.NoAbstractTexture;
 import net.ludocrypt.fogbox.shader.PatchedSampler;
 import net.ludocrypt.fogbox.shader.ShaderPatch;
@@ -20,28 +19,8 @@ public class Fogbox implements ClientModInitializer {
 
 	@Override
 	public void onInitializeClient() {
-		FogboxConfig.init();
 		if (!FabricLoader.getInstance().isModLoaded("iris") || !FabricLoader.getInstance().isModLoaded("sodium") || !FabricLoader.getInstance().isModLoaded("optifine") || !FabricLoader.getInstance().isModLoaded("optifabric") || !FabricLoader.getInstance().isModLoaded("canvas")) {
-			ShaderPatchManager.INSTANCE.addToAll(ShaderPatch.builder()
-					.sampler(SKYBOX_SAMPLER)
-					.fragment()
-					.declare("#moj_import <shift_to_skybox.glsl>")
-					.wrapCall("linear_fog",
-							"shift_to_skybox_from_fog",
-							"color",
-							"vertexDistance",
-							"FogStart",
-							"FogEnd",
-							"gl_FragCoord",
-							SKYBOX_SAMPLER.samplerName)
-					.end()
-					.build(),
-					FogboxConfig.getInstance().enableClouds ? "position_tex_color_normal" : "",
-					"rendertype_solid",
-					"rendertype_cutout",
-					"rendertype_cutout_mipped",
-					"rendertype_translucent",
-					"rendertype_tripwire");
+			ShaderPatchManager.INSTANCE.addToAll(ShaderPatch.builder().sampler(SKYBOX_SAMPLER).fragment().declare("#moj_import <shift_to_skybox.glsl>").wrapCall("linear_fog", "shift_to_skybox_from_fog", "color", "vertexDistance", "FogStart", "FogEnd", "gl_FragCoord", SKYBOX_SAMPLER.samplerName).end().build(), "rendertype_solid", "rendertype_cutout", "rendertype_cutout_mipped", "rendertype_translucent", "rendertype_tripwire");
 		} else {
 			LOGGER.warn("Conflicting rendering mod loaded, skybox rendering ceasing!");
 			isRenderingMod = true;
--- src/main/java/net/ludocrypt/fogbox/mixin/WorldRendererMixin.java
+++ src/main/java/net/ludocrypt/fogbox/mixin/WorldRendererMixin.java
@@ -6,6 +6,7 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.mojang.blaze3d.systems.RenderSystem;
@@ -15,8 +16,10 @@ import net.ludocrypt.fogbox.access.WorldRendererAccess;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gl.Framebuffer;
 import net.minecraft.client.gl.SimpleFramebuffer;
+import net.minecraft.client.gl.VertexBuffer;
 import net.minecraft.client.render.BufferBuilderStorage;
-import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.render.Camera;
+import net.minecraft.client.render.Shader;
 import net.minecraft.client.render.WorldRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.client.world.ClientWorld;
@@ -29,44 +32,45 @@ public abstract class WorldRendererMixin implements WorldRendererAccess {
 	private ClientWorld world;
 
 	@Shadow
+	private VertexBuffer lightSkyBuffer;
+
+	@Shadow
+	private VertexBuffer darkSkyBuffer;
+
+	@Shadow
 	@Final
 	private MinecraftClient client;
 
 	@Unique
 	private Framebuffer skyboxBuffer;
 
-	@Unique
-	private boolean writingToBuffer = true;
-
 	@Inject(method = "<init>", at = @At("TAIL"))
 	private void fogbox$init(MinecraftClient client, BufferBuilderStorage bufferBuilders, CallbackInfo ci) {
 		this.skyboxBuffer = new SimpleFramebuffer(this.client.getWindow().getFramebufferWidth(), this.client.getWindow().getFramebufferHeight(), true, MinecraftClient.IS_SYSTEM_MAC);
 	}
 
-	@Inject(method = "Lnet/minecraft/client/render/WorldRenderer;renderSky(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/util/math/Matrix4f;FLjava/lang/Runnable;)V", at = @At("HEAD"))
-	private void fogbox$renderSky$head(MatrixStack matrices, Matrix4f projectionMatrix, float tickDelta, Runnable runnable, CallbackInfo ci) {
-		if (this.writingToBuffer) {
-			this.skyboxBuffer.beginWrite(false);
-			RenderSystem.disableBlend();
-			RenderSystem.clear(16640, MinecraftClient.IS_SYSTEM_MAC);
-		}
+	@Inject(method = "Lnet/minecraft/client/render/WorldRenderer;renderSky(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/util/math/Matrix4f;FLnet/minecraft/client/render/Camera;ZLjava/lang/Runnable;)V", at = @At("HEAD"))
+	private void fogbox$renderSky$head(MatrixStack matrices, Matrix4f projectionMatrix, float tickDelta, Camera camera, boolean bl, Runnable runnable, CallbackInfo ci) {
+		this.skyboxBuffer.beginWrite(false);
+		RenderSystem.disableBlend();
+		RenderSystem.clear(16640, MinecraftClient.IS_SYSTEM_MAC);
 	}
 
-	@Inject(method = "Lnet/minecraft/client/render/WorldRenderer;renderSky(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/util/math/Matrix4f;FLjava/lang/Runnable;)V", at = @At("RETURN"))
-	private void fogbox$renderSky$tail(MatrixStack matrices, Matrix4f projectionMatrix, float tickDelta, Runnable runnable, CallbackInfo ci) {
-		if (this.writingToBuffer) {
-			this.skyboxBuffer.endWrite();
-			MinecraftClient.getInstance().getFramebuffer().beginWrite(false);
-			Fogbox.SKYBOX_SAMPLER.texture = this.skyboxBuffer;
-			this.writingToBuffer = false;
-			RenderSystem.setShader(GameRenderer::getPositionShader);
-			this.renderSky(matrices, projectionMatrix, tickDelta, runnable);
-			this.writingToBuffer = true;
-		}
+	@Inject(method = "Lnet/minecraft/client/render/WorldRenderer;renderSky(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/util/math/Matrix4f;FLnet/minecraft/client/render/Camera;ZLjava/lang/Runnable;)V", at = @At("RETURN"))
+	private void fogbox$renderSky$tail(MatrixStack matrices, Matrix4f projectionMatrix, float tickDelta, Camera camera, boolean bl, Runnable runnable, CallbackInfo ci) {
+		this.skyboxBuffer.endWrite();
+		Framebuffer framebuffer = this.client.getFramebuffer();
+		framebuffer.beginWrite(false);
+		Fogbox.SKYBOX_SAMPLER.texture = this.skyboxBuffer;
+		this.skyboxBuffer.draw(this.client.getWindow().getFramebufferWidth(), this.client.getWindow().getFramebufferHeight());
+		framebuffer.copyDepthFrom(this.skyboxBuffer);
+		framebuffer.beginWrite(false);
 	}
 
-	@Shadow
-	public abstract void renderSky(MatrixStack matrices, Matrix4f projectionMatrix, float tickDelta, Runnable runnable);
+	@Redirect(method = "Lnet/minecraft/client/render/WorldRenderer;renderSky(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/util/math/Matrix4f;FLnet/minecraft/client/render/Camera;ZLjava/lang/Runnable;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gl/VertexBuffer;setShader(Lnet/minecraft/util/math/Matrix4f;Lnet/minecraft/util/math/Matrix4f;Lnet/minecraft/client/render/Shader;)V", ordinal = 2))
+	private void fogbox$renderDarkSky(VertexBuffer darkSkyBuffer, Matrix4f viewMatrix, Matrix4f projectionMatrix, Shader shader, MatrixStack matrices, Matrix4f projectionMatrixIn, float tickDelta, Camera cameraIn, boolean blIn, Runnable runnableIn) {
+		// Stop doing that
+	}
 
 	@Override
 	public void updateSkyboxResolution() {
