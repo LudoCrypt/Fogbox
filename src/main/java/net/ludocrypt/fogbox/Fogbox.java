88cd5d3f4e80f5a64a55bbd9cafef12fe805d226
LudoCrypt <60561627+LudoCrypt@users.noreply.github.com>
2022-06-11 17:40:39 -0500

    actually fixes #3

--- src/main/java/net/ludocrypt/fogbox/Fogbox.java
+++ src/main/java/net/ludocrypt/fogbox/Fogbox.java
@@ -15,11 +15,13 @@ public class Fogbox implements ClientModInitializer {
 	public static final Logger LOGGER = LogManager.getLogger("Fogbox");
 	public static final PatchedSampler SKYBOX_SAMPLER = new PatchedSampler("SkyboxSampler", new NoAbstractTexture());
 
-	public static final boolean RENDERING_MOD = (FabricLoader.getInstance().isModLoaded("irisshaders") || FabricLoader.getInstance().isModLoaded("optifine") || FabricLoader.getInstance().isModLoaded("optifabric") || FabricLoader.getInstance().isModLoaded("canvas"));
+	public static final boolean RENDERING_MOD = (FabricLoader.getInstance().isModLoaded("iris") || FabricLoader.getInstance().isModLoaded("optifine") || FabricLoader.getInstance().isModLoaded("optifabric") || FabricLoader.getInstance().isModLoaded("canvas"));
 
 	@Override
 	public void onInitializeClient() {
-		ShaderPatchManager.INSTANCE.addToAll(ShaderPatch.builder().sampler(SKYBOX_SAMPLER).fragment().declare("#moj_import <shift_to_skybox.glsl>").wrapCall("linear_fog", "shift_to_skybox_from_fog", "color", "vertexDistance", "FogStart", "FogEnd", "gl_FragCoord", SKYBOX_SAMPLER.samplerName).end().build(), "rendertype_solid", "rendertype_cutout", "rendertype_cutout_mipped", "rendertype_translucent", "rendertype_tripwire");
+		if (!RENDERING_MOD) {
+			ShaderPatchManager.INSTANCE.addToAll(ShaderPatch.builder().sampler(SKYBOX_SAMPLER).fragment().declare("#moj_import <shift_to_skybox.glsl>").wrapCall("linear_fog", "shift_to_skybox_from_fog", "color", "vertexDistance", "FogStart", "FogEnd", "gl_FragCoord", SKYBOX_SAMPLER.samplerName).end().build(), "rendertype_solid", "rendertype_cutout", "rendertype_cutout_mipped", "rendertype_translucent", "rendertype_tripwire");
+		}
 	}
 
 }
